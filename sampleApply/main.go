f8b65f6f4b6fec294044aa8968dd989b4534dda1
Dmitry Kulikovsky <Kulikovsky Dmitry>
2016-09-02 18:11:26 +0300

    fixed indent

--- sampleApply/main.go
+++ sampleApply/main.go
@@ -13,11 +13,12 @@ import (
 )
 
 type ApplyResponse struct {
-    GroupId string
-    Exception string
+	GroupId   string
+	Exception string
 }
 
 var capi_url string = "http://sit-dev-01-sas.haze.yandex.net:8081/proto/v0/state/full"
+
 //var capi_url string "http://iss00-prestable.search.yandex.net:8082/proto/v0/state/full"
 
 func main() {
@@ -27,20 +28,20 @@ func main() {
 		Priority:  100,
 		ProjectId: "CAPIDEVNETS",
 	}
-    cstate := state.GetCompactState(capi_url)
-    state.PrintState(cstate)
+	cstate := state.GetCompactState(capi_url)
+	state.PrintState(cstate)
 	run_sample_workload(cstate, owner)
 }
 
 func run_sample_workload(cstate []*state.Host, owner *clusterapi.Owner) {
 	workload := capi.SampleWorkload(owner)
-    // predefined host, but with real etag from cstate
+	// predefined host, but with real etag from cstate
 	host := "s1-1106.qloud.yandex.net"
-    host_etag, ok := get_host_etag(host, cstate)
-    if !ok {
-        log.Printf("Failed to find host %s in cstate", host)
-    }
-    log.Printf("got host %s etag %d", host, host_etag)
+	host_etag, ok := get_host_etag(host, cstate)
+	if !ok {
+		log.Printf("Failed to find host %s in cstate", host)
+	}
+	log.Printf("got host %s etag %d", host, host_etag)
 
 	// update workload params
 	// get ip and hostname from ip-broker
@@ -56,7 +57,7 @@ func run_sample_workload(cstate []*state.Host, owner *clusterapi.Owner) {
 
 	// send apply request to capi
 	client := &http.Client{}
-    req, err := http.NewRequest("POST", capi_url, bytes.NewBuffer(data))
+	req, err := http.NewRequest("POST", capi_url, bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal("failed to create new request: ", err)
 	}
@@ -79,26 +80,26 @@ func run_sample_workload(cstate []*state.Host, owner *clusterapi.Owner) {
 	for key, value := range hdr {
 		fmt.Println("   ", key, ":", value)
 	}
-    log.Printf("body %s", body)
-    log.Printf("parse response obj %v", parse_apply_res(body))
+	log.Printf("body %s", body)
+	log.Printf("parse response obj %v", parse_apply_res(body))
 	log.Println("successfully marshaled empty container obj")
 }
 
 func get_host_etag(host string, cstate []*state.Host) (int64, bool) {
-    for _,h := range cstate {
-        if h.Health == "UP" && h.Id == host {
-            return h.Etag, true
-        }
-    }
-    return 0, false
+	for _, h := range cstate {
+		if h.Health == "UP" && h.Id == host {
+			return h.Etag, true
+		}
+	}
+	return 0, false
 }
 
 func parse_apply_res(body []byte) []*ApplyResponse {
-    result := make([]*ApplyResponse,0)
+	result := make([]*ApplyResponse, 0)
 	// unmarshal request
 	raw := new(clusterapi.ApplyGroupTransitionResponse)
 	if err := proto.Unmarshal(body, raw); err != nil {
 		log.Fatal("failed to unmarshal response:", err)
 	}
-    return result
+	return result
 }
